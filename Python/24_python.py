f247d0d64d3248980c6487b77107b1b825d52c94
Nikunj Parmar <nikunjparmar828@gmail.com>
2017-10-31 00:37:22 +0530

    24_python.py

--- Python/24_python.py
+++ Python/24_python.py
@@ -1,11 +1,10 @@
 from urllib import request
 
-goog_url = http://real-chart.finance.yahoo.com/table.csv?s=GOOG&d=2&e=8&f=2015&g=d&a=2&b=27&c=2014&ignore=.csv
+goog_url = 'http://real-chart.finance.yahoo.com/table.csv?s=GOOG&d=2&e=8&f=2015&g=d&a=2&b=27&c=2014&ignore=.csv'
 
 def download_stock_data(csv_url):
     response = request.urlopen(csv_url)
-    csv = response.read()
-    csv_str = str(csv)
+    csv_str = str(response.read())          #You(Bucky) tought us to simplify stuff as much as we can
     lines = csv_str.split("\\n")
     dest_url = r'goog.csv'
     fx = open(dest_url, "w")
